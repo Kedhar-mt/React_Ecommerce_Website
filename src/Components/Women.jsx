0b3f12394816111291f6a62a470349963fc56417
Kedhareswar <129578151+Kedhar-mt@users.noreply.github.com>
2024-10-31 23:07:55 +0530

    Update Women.jsx

--- src/Components/Women.jsx
+++ src/Components/Women.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card'; // Ensure proper casing for import
+import Card from './Card.jsx'; // Ensure proper casing for import
 import { PRODUCTS } from '../products'; // Import the products array
 
 const Women = () => {
